c3e9a6fd7501f88b9b353de1727bc727b5d63222
SkyWhy14 <artursgravitis312@gmail.com>
2025-09-23 12:19:36 +0300

    viss izveidots prieks izvelei

--- src/Uzd5/Centrs.java
+++ src/Uzd5/Centrs.java
@@ -1,15 +1,23 @@
 package Uzd5;
 
 public class Centrs {
+	
 	//Atributi
 	public int x,y;
+	
 	//Konstruktors
+	
  	public Centrs(int x, int y) {
  	this.x = x;
  	this.y = y;
  	}
+ 	
  	//Metodes	
- 	public String izvadKordinatas() {
- 		return "Centra koordinatas: (" + x + "," + y + ")";
+ 	public String izvadKoordinatas() {
+ 		return "Centra punkts atrodas: (x = " + x + ",y = " + y + ")";
  	}
+    public void parvietot(int koordX, int koordY) {
+		x += koordX;
+		y += koordY;
+	}
 }
--- src/Uzd5/Figuras.java
+++ src/Uzd5/Figuras.java
@@ -3,8 +3,29 @@ package Uzd5;
 public class Figuras {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Izveido Centrs objektu
+		Centrs CentraP1 = new Centrs(2,3);
+		System.out.println(CentraP1.izvadKoordinatas());
+		CentraP1.parvietot(3, -1);
+        System.out.println(CentraP1.izvadKoordinatas());
+        //Izvedo cetrsturis objektu
+        
+        cetrsturis cetrsturis1 = new cetrsturis();
+        System.out.println(cetrsturis1.izvaditCentru());
+        //izvadit si cetrstura atrasanas vietu x un y 
+        System.out.println(cetrsturis1.punkts.izvadKoordinatas());
+        
+        cetrsturis cetrsturis2 = new cetrsturis(CentraP1,6,7);
+        System.out.println(cetrsturis2.izvaditCentru()+"\n"+
+        		cetrsturis2.punkts.izvadKoordinatas()+"\nLaukums = "+cetrsturis2.Laukums()+"cm^2");
+        
+        
+        Centrs punkts2 = new Centrs(5,5);
+        aplis aplis1 = new aplis(punkts2,5);
+        System.out.println("Rinka "+aplis1.izvaditAplaInfo()+"\n"+
+				"Rinka linija: "+aplis1.Rlinija()+"cm\n"+
+				"Rinka laukums: "+aplis1.Rlaukums()+"cm^2\n"+
+				"Rinka centrs: "+aplis1.punkts.izvadKoordinatas());
 	}
 
 }
