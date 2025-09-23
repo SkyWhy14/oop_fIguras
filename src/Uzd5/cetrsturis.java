b060d31e6385a538665f1557539fd1f2e79fab42
SkyWhy14 <artursgravitis312@gmail.com>
2025-09-23 11:35:40 +0300

    Pabeigta klase cetrsturis

--- src/Uzd5/cetrsturis.java
+++ src/Uzd5/cetrsturis.java
@@ -1,5 +1,24 @@
 package Uzd5;
 
 public class cetrsturis {
-
+  public double platums,augstums;
+  public Centrs punkts;
+  
+  
+  //Konstruktoru parslogosana
+  public cetrsturis() {
+	  punkts = new Centrs(0,0);
+  }
+  public cetrsturis(Centrs punkts,double p,double a) {
+	 this.punkts = punkts;
+	 platums = p;
+	 augstums = a;
+  }
+  public String izvaditCentru() {
+	  return "Platums: " + platums + "cm augtums: " + augstums + "cm ";
+  }
+  //Uztaisit metodi kas aprekina laukumu
+  public double Laukums() {
+	  return platums * augstums;
+  }
 }
